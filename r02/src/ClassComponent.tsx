0ac5150482434cdb28875d6c39c12c5718d607ec
aviation5770 <aviation5770@gmail.com>
2025-09-28 20:10:42 +0900

    fix: 축약 주석 추가

--- r02/src/ClassComponent.tsx
+++ r02/src/ClassComponent.tsx
@@ -9,6 +9,7 @@ export default class ClassComponent extends Component<ClassComponentProps> {
   render() {
     const href = this.props.href
     const text = this.props.text
+    // const {href, text} = this.props; 로도 축약 가능
 
     return (
       <li>
