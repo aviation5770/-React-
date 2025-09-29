2f87650d73301f34029d1d8dd2baca23478fc784
aviation5770 <aviation5770@gmail.com>
2025-09-29 23:35:27 +0900

    feat: key 배우기

--- r02/src/App.tsx
+++ r02/src/App.tsx
@@ -2,10 +2,6 @@ import ArrowComponent from "./ArrowComponent"
 import ClassComponent from "./ClassComponent"
 
 export default function App () {
-    return (
-      <ul>
-        <ClassComponent href="https://www.google.com" text="Go to google" />
-        <ArrowComponent href="https://www.naver.com" text="Go to naver"/>
-      </ul>
-    )
+  const text = [<p>Hello</p>, <p>Would</p>]  
+  return <div>{text}</div>
 }
