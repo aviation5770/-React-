f40d870590281f10c48c367f440e225198ca77ea
aviation5770 <aviation5770@gmail.com>
2025-09-30 20:29:01 +0900

    feat: EventListener 구현

--- r03/src/App.tsx
+++ r03/src/App.tsx
@@ -1,34 +1,30 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import EventListener from './pages/EventListener'
+import OnClick from './pages/OnClick'
+import ReactOnClick from './pages/ReactOnClick'
+import DispatchEvent from './pages/DispatchEvent'
+import EventBubbling from './pages/EventBubbling'
+import StopPropagation from './pages/StopPropagation'
+import VariousInputs from './pages/VariousInputs'
+import OnChange from './pages/Onchange'
+import FileInput from './pages/FileInput'
+import DragDrop from './pages/DragDrop'
+import FileDrop from './pages/FileDrop'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+      <FileDrop />
+      <DragDrop />
+      <FileInput />
+      <OnChange />
+      <VariousInputs />
+      <StopPropagation />
+      <EventBubbling />
+      <DispatchEvent />
+      <ReactOnClick />
+      <OnClick />
+      <EventListener />
+    </div>
   )
 }
 
--- r03/src/pages/EventListener.tsx
+++ r03/src/pages/EventListener.tsx
@@ -1,3 +1,15 @@
-export default function CopyMe() {
-  return <div>CopyMe</div>
+// 2개 이벤트 리스너 등록
+
+document.getElementById('root')?.addEventListener('click', (e: Event) => {
+  const { isTrusted, target, bubbles } = e
+  console.log('mouse click occurs', isTrusted, target, bubbles)
+})
+
+document.getElementById('root')?.addEventListener('click', (e: Event) => {
+  const { isTrusted, target, bubbles } = e
+  console.log('mouse click also occurs', isTrusted, target, bubbles)
+})
+
+export default function EventListener() {
+  return <div>EventListener</div>
 }
