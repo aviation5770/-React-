9cb3e91d7e292aba441448860158095b18f4b9df
aviation5770 <aviation5770@gmail.com>
2025-10-05 09:24:39 +0900

    feat: 이벤트 버블링 학습

--- r03/src/pages/StopPropagation.tsx
+++ r03/src/pages/StopPropagation.tsx
@@ -1,3 +1,15 @@
-export default function CopyMe() {
-  return <div>CopyMe</div>
+import type { SyntheticEvent } from "react"
+
+export default function EventListener() {
+  const onDivClick = (e : SyntheticEvent) => console.log('click event on <div>')
+  const onButtonClick = (e : SyntheticEvent) => {
+    console.log('click event on <button>')
+    e.stopPropagation()
+  }
+  return (
+    <div onClick={onDivClick}>
+      <p>stop Propagation</p>
+      <button onClick={onButtonClick}>Click Me and stop event Propagation</button>
+    </div>
+  )
 }
