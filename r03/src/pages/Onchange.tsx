b85edf5b93f1f14965c8a5a64998be3ce8944c16
aviation5770 <aviation5770@gmail.com>
2025-10-06 14:14:44 +0900

    feat: OnChange 주석 포함 추가

--- r03/src/pages/Onchange.tsx
+++ r03/src/pages/Onchange.tsx
@@ -1,3 +1,26 @@
-export default function CopyMe() {
-  return <div>CopyMe</div>
+import type { ChangeEvent } from "react"
+
+export default function Onchange() {
+  const onChangeValue = (e: ChangeEvent<HTMLInputElement>) => {
+    e.stopPropagation()
+    e.preventDefault()
+    console.log('onChangeValue', e.target.value)
+  }
+  const onChangeChecked = (e: ChangeEvent<HTMLInputElement>) => {
+    e.stopPropagation()
+    console.log('onChangeChecked', e.target.checked)
+  }
+  const onChangeFiles = (e: ChangeEvent<HTMLInputElement>) => {
+    e.stopPropagation()
+    console.log('onChangeFiles', e.target.files)
+  }
+
+  return (
+    <div><p>OnChange</p>
+      <input type="text" onChange={onChangeValue} defaultValue='Hello'/>
+      <input type="checkbox" onChange={onChangeChecked} defaultChecked />
+      <input type="file" onChange={onChangeFiles} multiple accept="image/*" />
+      {/* 파일 선택 화면에서 cmd + click으로 여러 파일 선택 가능 and 그림 파일만 선택 가능 */}
+    </div>
+  )
 }
