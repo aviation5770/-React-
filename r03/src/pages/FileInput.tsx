688a4f78b35b79e2da32838bb26c6c31f952a88a
aviation5770 <aviation5770@gmail.com>
2025-10-07 18:24:15 +0900

    feat: file onChange 이벤트 처리 추가

--- r03/src/pages/FileInput.tsx
+++ r03/src/pages/FileInput.tsx
@@ -1,3 +1,19 @@
-export default function CopyMe() {
-  return <div>CopyMe</div>
+import type { ChangeEvent } from "react"
+
+export default function FileInput() {
+  const onChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const files: FileList | null = e.target.files
+    if (files) {
+      for (let i = 0; i < files.length; i++) {
+        const file: File | null = files.item(i)
+        console.log(`files[${i}]`, file)
+      }
+    }
+  }
+  return (
+    <div>
+      <p>File Input</p>
+      <input type="file" onChange={onChange} multiple accept="image/*" />
+    </div>
+  )
 }
