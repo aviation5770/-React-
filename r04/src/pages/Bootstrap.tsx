562de5edf7c37443f842be964937a9b040ec774a
aviation5770 <aviation5770@gmail.com>
2025-10-11 09:05:26 +0900

    feat: Bootstrap 추가

--- r04/src/pages/Bootstrap.tsx
+++ r04/src/pages/Bootstrap.tsx
@@ -1,3 +1,27 @@
-export default function CopyMe() {
-  return <div>CopyMe</div>
+export default function BootStrap() {
+  return (
+    <form>
+      <div className="mb-3">
+        <label htmlFor="exampleInputEmail1" className="form-label">Email address</label>
+        <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
+        <div id="emailHelp" className="form-text">We'll never share your email with anyone else.</div>
+      </div>
+      <div className="mb-3">
+        <label htmlFor="exampleInputPassword1" className="form-label">Password</label>
+        <input type="password" className="form-control" id="exampleInputPassword1" />
+      </div>
+      <div className="mb-3 form-check">
+        <input type="checkbox" className="form-check-input" id="exampleCheck1" />
+        <label className="form-check-label" htmlFor="exampleCheck1">Check me out</label>
+      </div>
+      <button type="submit" className="btn btn-primary">Submit</button>
+    </form>
+  )
 }
+
+{/* 
+Bootstrap 코드 가져오는 방법
+1. Bootstrap 문서에서 [Forms -> Overview]를 클릭
+2. 본문에서 Overview 단원 아래에 있는 HTML 코드를 복사
+3. return 문 뒤에 붙여넣기
+*/}
