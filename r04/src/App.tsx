c8343f840ad71f8035c49bf9a908166bc95a7772
aviation5770 <aviation5770@gmail.com>
2025-10-13 10:06:36 +0900

    feat: className 추가

--- r04/src/App.tsx
+++ r04/src/App.tsx
@@ -13,6 +13,15 @@ function App() {
       <Icon />
       <BootStrap />
       <button className="btn btn-primary">TEST</button>
+
+      {/* CSS 기본구문 실습 */}
+      <div>
+        <div className="box">box</div> {/* class는 JS 예약어이므로 충돌을 피하기 위해 Name을 붙혀 사용 */}
+          <button className="btn">Click me!</button>
+          <div className="outer">
+          <div className="inner">Hello CSS</div>
+        </div>
+      </div>
     </div>
   );
 }
