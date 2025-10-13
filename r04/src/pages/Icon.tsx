1987623b9b78779e5fcb1d41cefe1d2b91af0edc
aviation5770 <aviation5770@gmail.com>
2025-10-13 12:35:12 +0900

    feat: import 규칙으로 Icon 수정

--- r04/src/pages/Icon.tsx
+++ r04/src/pages/Icon.tsx
@@ -1,3 +1,9 @@
-export default function CopyMe() {
-  return <div>CopyMe</div>
+export default function Icon() {
+  return (
+    <div>
+      <h3>Icon</h3>
+      <span className="material-icons">delete</span>
+      <span className="material-icons">check_circle_outline</span>
+    </div>
+  );
 }
