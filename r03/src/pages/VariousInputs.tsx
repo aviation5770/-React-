547d225388079698751cb06e7c226f1ba0ca72a3
aviation5770 <aviation5770@gmail.com>
2025-10-17 11:17:01 +0900

    feat: <input>요소 타입 정리

--- r03/src/pages/VariousInputs.tsx
+++ r03/src/pages/VariousInputs.tsx
@@ -1,3 +1,18 @@
-export default function CopyMe() {
-  return <div>CopyMe</div>
+export default function VariousInputs() {
+  return (
+    <div>
+      <p>VariousInputs</p>
+      <input type="text" placeholder="Enter some text" />
+      <input type="password" placeholder="Enter your password" />
+      <input type="email" placeholder="Enter your email" />
+      <input type="number" placeholder="Enter your age" min={0} />
+      <input type="range" />
+      <input type="button" value="Button" />
+      <input type="checkbox" value="defaultChecked" />
+      <input type="checkbox" value="" />
+      <input type="radio" value="adult" name="isAdult" defaultChecked />
+      <input type="radio" value="not-adult" name="isAdult" />
+      <input type="file" />
+    </div>
+  )
 }
