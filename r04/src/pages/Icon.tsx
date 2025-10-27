a1339fa9f6421051905735a5d736e91fe5403701
aviation5770 <aviation5770@gmail.com>
2025-10-27 16:37:56 +0900

    feat: 부모가 전달해준 style 객체를 사용할 수 있도록 개선

--- r04/src/pages/Icon.tsx
+++ r04/src/pages/Icon.tsx
@@ -1,9 +1,31 @@
-export default function Icon() {
+// 기존 코드
+
+// export default function Icon() {
+//   return (
+//     <div>
+//       <h3>Icon</h3>
+//       <span className="material-icons">delete</span>
+//       <span className="material-icons">check_circle_outline</span>
+//     </div>
+//   );
+// }
+
+
+
+// 코드 개선해보기
+
+import type { FC, CSSProperties } from "react";
+
+export type IconProps = {
+  name: string;
+  style?: CSSProperties;
+};
+
+export const Icon: FC<IconProps> = (props) => {
+  const { name, style } = props;
   return (
-    <div>
-      <h3>Icon</h3>
-      <span className="material-icons">delete</span>
-      <span className="material-icons">check_circle_outline</span>
-    </div>
+    <span className="material-icons" style={style}>{name}</span>
   );
-}
+};
+
+// 부모가 전달해준 style 객체를 사용할 수 있도록 개선
