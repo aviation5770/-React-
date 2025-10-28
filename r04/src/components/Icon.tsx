a2f48ee2665ba5e9bf9f984ebc450ee7a5c4ea79
aviation5770 <aviation5770@gmail.com>
2025-10-28 16:45:31 +0900

    fix: Icon 코드 개선

--- r04/src/components/Icon.tsx
+++ r04/src/components/Icon.tsx
@@ -1,6 +1,20 @@
-import type { FC } from "react";
+// import type { FC } from "react";
 
-export type IconProps = { name: string };
+// export type IconProps = { name: string };
+// export const Icon: FC<IconProps> = (props) => {
+//   return <span className="material-icons">{props.name}</span>;
+// }
+
+import type { FC, CSSProperties } from "react";
+
+export type IconProps = {
+  name: string;
+  style?: CSSProperties;
+};
+
 export const Icon: FC<IconProps> = (props) => {
-  return <span className="material-icons">{props.name}</span>;
-}
+  const { name, style } = props;
+  return (
+    <span className="material-icons" style={style}>{name}</span>
+  );
+};
--- r04/src/pages/Icon.tsx
+++ r04/src/pages/Icon.tsx
@@ -1,31 +1,9 @@
-// 기존 코드
-
-// export default function Icon() {
-//   return (
-//     <div>
-//       <h3>Icon</h3>
-//       <span className="material-icons">delete</span>
-//       <span className="material-icons">check_circle_outline</span>
-//     </div>
-//   );
-// }
-
-
-
-// 코드 개선해보기
-
-import type { FC, CSSProperties } from "react";
-
-export type IconProps = {
-  name: string;
-  style?: CSSProperties;
-};
-
-export const Icon: FC<IconProps> = (props) => {
-  const { name, style } = props;
+export default function Icon() {
   return (
-    <span className="material-icons" style={style}>{name}</span>
+    <div>
+      <h3>Icon</h3>
+      <span className="material-icons">delete</span>
+      <span className="material-icons">check_circle_outline</span>
+    </div>
   );
-};
-
-// 부모가 전달해준 style 객체를 사용할 수 있도록 개선
+}
