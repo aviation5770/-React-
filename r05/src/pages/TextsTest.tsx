248e50f70a87d40ace2eaf8ea5717e2ce2a7eba1
aviation5770 <aviation5770@gmail.com>
2025-11-03 17:18:42 +0900

    fix: text test 코드에 subtitle 추가

--- r05/src/pages/TextsTest.tsx
+++ r05/src/pages/TextsTest.tsx
@@ -1,9 +1,14 @@
-import { Title } from "../components";
+import * as D from "../data";
+import { Title, Subtitle } from "../components";
 
 export default function TextsTest() {
   return (
     <div>
       <Title>TextsTest</Title>
+      <div>
+        <Title className="text-blue-600">{D.randomTitleText()}</Title>
+        <Subtitle className="text-blue-400">{D.randomSentence()}</Subtitle>
+      </div>
     </div>
   );
-};
+}
