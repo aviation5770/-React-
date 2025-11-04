180237019bd57954c1b5b3021e5d45fc029ad46b
aviation5770 <aviation5770@gmail.com>
2025-11-04 15:09:16 +0900

    feat: 코딩규칙 및 css 설정파일 추가

---
+++ r06/.prettierrc.cjs
@@ -0,0 +1,9 @@
+module.exports = {
+  bracketSpacing : false,       // 객체 리터럴에서 중괄호 안에 공백을 넣을지 여부
+  jsxBracketSameLine : true,    // 여러 줄 JSX에서 마지막 >를 같은 줄에 둘지 여부
+  singleQuote: true,            // 문자열을 '작은따옴표'로 통일
+  trailingComma : none,        // 여러 줄일 때 마지막 요소 뒤에 ,를 붙일지 여부
+  arrowParens : 'avoid',        // 화살표 함수 매개변수 괄호 처리(하나의 매개변수일때 괄호 생략)
+  semi: false,                  // 문장 끝에 세미콜론(;) 붙일지 여부
+  printWidth: 90,               // 한 줄 최대 길이 (90을 넘어가면 자동 줄바꿈)
+};
