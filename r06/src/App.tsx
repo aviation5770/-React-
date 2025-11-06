7cf379ad8d9f832b7d9851b0ce1cd5ae81633b5a
aviation5770 <aviation5770@gmail.com>
2025-11-06 16:03:33 +0900

    feat: 컴포넌트 추가

--- r06/src/App.tsx
+++ r06/src/App.tsx
@@ -1,16 +1,33 @@
-import Tailwindcss from './pages/Tailwind';
-import Color from './pages/Color';
-import TextTest from './pages/TextsTest';
-import './App.css'
+import DivTest from "./pages/DivTest";
+import ViewportTest from "./pages/ViewportTest";
+import HeightTest from "./pages/HeightTest";
+import PaddingTest from "./pages/PaddingTest";
+import MarginTest from "./pages/MarginTest";
+import ImageTest from "./pages/ImageTest";
+import BackgroundImageTest from "./pages/BackgroundImageTest";
+import DisplayTest from "./pages/DisplayTest";
+import DisplayNoneTest from "./pages/DisplayNoneTest";
+import AvatarTest from "./pages/AvatarTest";
+import PositionTest from "./pages/PositionTest";
+import OverlayTest from "./pages/OverlayTest";
+
 
 function App() {
   return (
-    <div className='App'>
-      <TextTest />
-      <Color />
-      <Tailwindcss />
+    <div>
+      <OverlayTest />
+      <PositionTest />
+      <DisplayNoneTest />
+      <DisplayTest />
+      <AvatarTest />
+      <BackgroundImageTest />
+      <ImageTest />
+      <MarginTest />
+      <PaddingTest />
+      <HeightTest />
+      <ViewportTest />
+      <DivTest />
     </div>
-  )
+  );
 }
-
-export default App
+export default App;
